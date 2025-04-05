0303dbf77139594f30a61dd6b140912a180dc039
ogs1800 <ogechiaden1998@gmail.com>
2025-04-05 14:41:32 +0100

    adding 5 + 10 test

--- New app.py
+++ New app.py
@@ -1,5 +1,9 @@
 first = 'John'
 last = 'Smith'
-message = first + ' [' + last + '] is a coder
+message = first + ' [' + last + '] is a coder'
 msg = f'{first} [{last}] is a coder'
 print(message)
+
+x = 5
+y = 10
+print(x + y)
